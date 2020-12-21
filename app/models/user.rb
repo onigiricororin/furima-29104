2aa77dec55aae4cec089ece73355eb6e0505bfa5
onigiricororin <hirooo.f@gmail.com>
2020-12-21 22:36:59 +0900

    バリデーション修正(Pass最低文字数)

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,7 @@ class User < ApplicationRecord
     validates :birthday
     validates :email
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i ,
-              message: "は半角英数字混合で入力してください。"} ,
-              length: { minimum: 7 }  
+              message: "は半角英数字混合で入力してください。"} 
   end        
  
   
