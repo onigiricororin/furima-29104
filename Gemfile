84a511fce1614312a43c6dcbce9e277ecd8530b6
onigiricororin <hirooo.f@gmail.com>
2020-12-30 16:58:09 +0900

    activehash実装

--- Gemfile
+++ Gemfile
@@ -60,3 +60,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'rails-i18n'
 gem 'pry-rails'
+gem 'active_hash'
--- db/migrate/20201229064536_create_items.rb
+++ db/migrate/20201229064536_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.references :user,               null:false,foreign_key: true
+      t.references :user,               null:false,foreign_key:true
       t.string :name,                   null:false
       t.text :summary,                  null:false
       t.integer :price,                 null:false
