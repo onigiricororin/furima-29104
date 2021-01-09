99e55f081d0aee68d621dccab3464c9235415408
onigiricororin <hirooo.f@gmail.com>
2021-01-09 12:21:15 +0900

    リファクタリング・条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :find_params_id, only: [:show,:destroy]
 
   def index 
     @items = Item.all.order("created_at DESC")
@@ -19,17 +20,24 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if user_signed_in? && current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:image,:name,:summary,:price,:category_id,:condition_id,:burden_id,:prefecture_id,:days_to_ship_id).merge(user_id: current_user.id)
   end
+
+  def find_params_id
+    @item = Item.find(params[:id])
+  end
+
 end
