a9f0dff381a30e6bdeef52cab1a57fc621d43132
onigiricororin <hirooo.f@gmail.com>
2021-01-09 19:20:10 +0900

    エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,8 +26,11 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-    redirect_to item_path
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
 
