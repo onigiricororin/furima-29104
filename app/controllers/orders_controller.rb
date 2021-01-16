5844236d294eac49e6d9d91ead9d08495c27ecb2
onigiricororin <hirooo.f@gmail.com>
2021-01-16 23:54:22 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,16 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
-
  def index
+  @item = Item.find(params[:item_id])
   @order_address = OrderAddress.new
  end
 
  def create
-  @order = Order.new
-  @order_address = OrderAddress.new(address_params)
+  @item = Item.find(params[:item_id])
+  @order_address = OrderAddress.new(order_params)
   if @order_address.valid?
+    pay_item
     @order_address.save
     redirect_to root_path
   else
@@ -19,10 +20,16 @@ class OrdersController < ApplicationController
 
  private
 
- def address_params
- params.require(:order_address).permit(:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+ def order_params
+   params.require(:order_address).permit(:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
  end
 
-
-
+ def pay_item
+   Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # PAY.JPテスト秘密鍵
+   Payjp::Charge.create(
+     amount: @item.price,  # 商品の値段
+     card: order_params[:token],    # カードトークン
+     currency:'jpy' 
+   )
+ end
 end
