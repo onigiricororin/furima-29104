0af31e4aad81210923e44a2f65b2ff3e0084b5af
onigiricororin <hirooo.f@gmail.com>
2021-01-17 00:50:37 +0900

    コントローラーバリデーション

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,12 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :item_find
 
  def index
-  @item = Item.find(params[:item_id])
   @order_address = OrderAddress.new
  end
 
  def create
-  @item = Item.find(params[:item_id])
   @order_address = OrderAddress.new(order_params)
   if @order_address.valid?
     pay_item
@@ -19,6 +18,18 @@ class OrdersController < ApplicationController
  end
 
  private
+
+ def item_find
+  @item = Item.find(params[:item_id])
+    if @item.order.nil?
+      if @item.user_id == current_user.id
+       redirect_to root_path
+      end
+    else
+      redirect_to root_path
+    end
+ end
+
 
  def order_params
    params.require(:order_address).permit(:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
