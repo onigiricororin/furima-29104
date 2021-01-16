c65a5e4527a364b3e6ff12009a7443ba37ba6b23
onigiricororin <hirooo.f@gmail.com>
2021-01-16 23:54:07 +0900

    購入機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../calculate.js")
+require("../calculate")
+require("../payjp")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,8 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id,:item_id,:order_id,:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number
+  attr_accessor :user_id,:item_id,:order_id,:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number,:token
 
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "ハイフンが含まれていません"}
