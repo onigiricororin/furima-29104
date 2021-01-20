9f0e2592e82f964cac455341290b288648e9d1e1
onigiricororin <hirooo.f@gmail.com>
2021-01-20 21:35:31 +0900

    電話番号正規表現修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,7 +10,7 @@ class OrderAddress
     validates :prefecture_id,numericality: { other_than: 1, message: "が入力されていません"}
     validates :city
     validates :house_number
-    validates :phone_number, format: { with: /\A\d{11}\z/, message: "は半角数字11文字以内で入力してください。"} 
+    validates :phone_number, format: { with: /\A\d{1,11}\z/, message: "は半角数字11文字以内で入力してください。"} 
   end
 
   def save
