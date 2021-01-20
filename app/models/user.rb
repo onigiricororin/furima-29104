fa2434ec42f792dbc5e9d63c39e2fa869d025b12
onigiricororin <hirooo.f@gmail.com>
2021-01-20 20:34:35 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   has_many :orders
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   with_options presence: true do
     validates :nickname
     validates :birthday
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i ,
-              message: "は半角英数字混合で入力してください。"} 
+              message: "は6文字以上の半角英数字混合で入力してください。"} 
     with_options format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"} do
       validates :last_name 
       validates :first_name      
